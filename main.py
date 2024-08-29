d245324076eedf99cb088618f5cafe4275240887
Your NameDana <youremail@example.com23sweidanD@centeralfoundationboys.co.uk>
2024-08-29 18:39:07 +0100

    coins for bonus round

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 import pygame
 import sys
 import button
-import time
-
 
 pygame.init()
 
@@ -433,6 +431,15 @@ level2_coins = [(1, 9), (1, 8), (1, 7), (1, 6), (1, 4), (1, 3), (2, 3), (8, 3), 
                 (18, 8), (20, 3), (19, 3), (19, 4), (19, 5), (20, 5), (12, 3), (12, 4), (12, 5), (12, 6), (13, 6),
                 (18, 9), (20, 6),(20, 7), (10, 5), (10, 6), (10, 7)
                ]
+bonus_coins =[(15, 3), (15, 4), (15, 5), (15, 6), (15, 7), (15, 8), (16, 3), (16, 4), (16, 5), (16, 6), (16, 7),
+              (16, 8),
+              (5, 3), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8),
+              (7, 3), (7, 4), (7, 5), (7, 6), (7, 7), (7, 8), (8, 3), (8, 4), (8, 5), (8, 6), (8, 7), (8, 8),
+              (9, 3), (9, 4), (9, 5), (9, 6), (9, 7), (9, 8), (10, 3), (10, 4), (10, 5), (10, 6), (10, 7), (10, 8),
+              (11, 3), (11, 4), (11, 5), (11, 6), (11, 7), (11, 8), (12, 3), (12, 4), (12, 5), (12, 6), (12, 7),
+              (12, 8), (13, 3), (13, 4), (13, 5), (13, 6), (13, 7), (13, 8), (14, 3), (14, 4), (14, 5), (14, 6),
+              (14, 7), (14, 8)
+            ]
 
 # level1
 # function for placing the coins
@@ -460,6 +467,19 @@ def coin_collision2(sprite_x, sprite_y):
     sprite_pos = (sprite_x // TILESIZE, sprite_y // TILESIZE)
     if sprite_pos in level2_coins:
         level2_coins.remove(sprite_pos)
+        score2 += 50  # +50 points to the score
+
+def draw_coins_b():
+    for coin_pos in bonus_coins:
+        coin_x = coin_pos[0] * TILESIZE + 15
+        coin_y = coin_pos[1] * TILESIZE + 15
+        screen.blit(coin_image, (coin_x, coin_y))
+
+def coin_collision_b(sprite_x, sprite_y):
+    global score2
+    sprite_pos = (sprite_x // TILESIZE, sprite_y // TILESIZE)
+    if sprite_pos in bonus_coins:
+        bonus_coins.remove(sprite_pos)
         score2 += 50  # +50 points to the score
 
 
@@ -799,6 +819,8 @@ while run:
     elif state == BONUS_ROUND:
         draw_grid()
         display_sprite(sprite_x // TILESIZE, sprite_y // TILESIZE)
+        draw_coins_b()
+        coin_collision_b(sprite_x, sprite_y)
         start -= 0.5
         if start <= 0:
             state = LEVEL2
